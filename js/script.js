8bba30a2e757be448d2e515dbd1be68717dde0d6
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-07-22 17:59:36 +0900

    Updated.

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const alertBannar = document.getElementById("alert");
+alertBannar.innerHTML =
+`<strong>Alert </strong>This is an alert box.
+<span class="close-btn" onclick="this.parentElement.style.display='none';">&times;</span>`;
