e03099ca0c4a771e3188d97a4c83171129b1ba68
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-07-23 11:00:33 +0900

    Updated.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@ const alertBannar = document.getElementById("alert");
 alertBannar.innerHTML =
 `<strong>Alert </strong>This is an alert box.
 <span class="close-btn" onclick="this.parentElement.style.display='none';">&times;</span>`;
+
+let trafficCanvas = document.getElementById("");
