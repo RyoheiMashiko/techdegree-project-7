477de315420603c8c27577e2c9bf54201b2286ea
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-07-23 18:53:52 +0900

    Updated.

--- js/script.js
+++ js/script.js
@@ -111,12 +111,13 @@ const send = document.getElementById("send");
 send.addEventListener('click', () => {
   // ensure user and message fields are filled out
   if (user.value === "" && message.value === "") {
-  alert("Please fill out user and message fields before sending");
+    alert("Please fill out user and message fields before sending");
   } else if (user.value === "" ) {
-  alert("Please fill out user field before sending");
+    alert("Please fill out user field before sending");
   } else if (message.value === "" ) {
-  alert("Please fill out message field before sending");
+    alert("Please fill out message field before sending");
   } else {
-  alert(`Message successfully sent to: ${user.value}`);
+    alert(`Message successfully sent to: ${user.value}`);
   }
+  console.log("click");
   });
